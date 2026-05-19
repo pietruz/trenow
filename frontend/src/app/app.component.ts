bfc4278f1f236e548db09482be89e45c233615d8
Piero <p.aiuola@gmail.com>
2026-05-19 16:58:05 +0200

    desktop: X fermate visibile; info-btn in to-route apre train-detail

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -97,6 +97,12 @@ export class AppComponent implements AfterViewInit, OnDestroy {
     this.sidebarOpen.update(v => !v);
   }
 
+  openTrainDetail() {
+    if (this.isMobile()) {
+      this.sidebarOpen.set(true);
+    }
+  }
+
   closeSidebarMobile() {
     if (this.isMobile()) {
       this.sidebarOpen.set(false);
